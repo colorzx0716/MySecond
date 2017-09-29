5050856f61b7683730308a69174e631561f14d0f
colorzx0716 <31384907+colorzx0716@users.noreply.github.com>
2017-09-29 11:04:46 +0800

    Initial commit

---
+++ app/src/main/java/com/bawie/mysecond/CustomScanActivity.java
@@ -0,0 +1,113 @@
+package com.bawie.mysecond;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+public class CustomScanActivity extends AppCompatActivity implements DecoratedBarcodeView.TorchListener, View.OnClickListener {
+
+    private DecoratedBarcodeView mDBV;
+    private Button switchLight,btn_hint1,btn_hint2;
+
+    private CaptureManager captureManager;
+    private boolean isLightOn = false;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        captureManager.onPause();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        captureManager.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        captureManager.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        captureManager.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return mDBV.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scan);
+        mDBV = (DecoratedBarcodeView) findViewById(R.id.dbv_custom);
+        switchLight = (Button) findViewById(R.id.btn_switch);
+        btn_hint1 = (Button) findViewById(R.id.btn_hint1);
+        btn_hint2 = (Button) findViewById(R.id.btn_hint2);
+
+        switchLight.setOnClickListener(this);
+
+        mDBV.setTorchListener(this);
+
+        // 如果没有闪光灯功能，就去掉相关按钮
+        if(!hasFlash()) {
+            switchLight.setVisibility(View.GONE);
+        }
+
+        //重要代码，初始化捕获
+        captureManager = new CaptureManager(this,mDBV);
+        captureManager.initializeFromIntent(getIntent(),savedInstanceState);
+        captureManager.decode();
+
+    }
+
+
+
+    // 判断是否有闪光灯功能
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+
+
+    @Override
+    public void onTorchOn() {
+        Toast.makeText(this,"torch on",Toast.LENGTH_LONG).show();
+        isLightOn = true;
+    }
+
+    @Override
+    public void onTorchOff() {
+        Toast.makeText(this,"torch off",Toast.LENGTH_LONG).show();
+        isLightOn = false;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_switch:
+                if(isLightOn){
+                    mDBV.setTorchOff();
+                }else{
+                    mDBV.setTorchOn();
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/bawie/mysecond/MainActivity.java
@@ -0,0 +1,77 @@
+package com.bawie.mysecond;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String str = "123";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    // 你也可以使用简单的扫描功能，但是一般扫描的样式和行为都是可以自定义的，这里就写关于自定义的代码了
+    // 你可以把这个方法作为一个点击事件
+    public void customScan(View view){
+        new IntentIntegrator(this)
+                .setOrientationLocked(false)
+                .setCaptureActivity(CustomScanActivity.class) // 设置自定义的activity是CustomActivity
+                .initiateScan(); // 初始化扫描
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null) {
+            if(intentResult.getContents() == null) {
+                Toast.makeText(this,"内容为空",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,"扫描成功",Toast.LENGTH_LONG).show();
+                // ScanResult 为 获取到的字符串
+                String ScanResult = intentResult.getContents();
+            }
+        } else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+
+    }
+
+    //传入一个字符串，生成一个二维码的Bitmap，可以用于显示
+    Bitmap encodeAsBitmap(String str) {
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(str, BarcodeFormat.QR_CODE, 200, 200);
+            // 使用 ZXing Android Embedded 要写的代码
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            bitmap = barcodeEncoder.createBitmap(result);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException iae) {
+            return null;
+        }
+        return bitmap;
+    }
+
+
+}
